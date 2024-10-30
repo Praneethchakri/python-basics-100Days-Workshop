3bbdde53c120ed74e4a5afa92a07f36a9d643b1c
unknown <praneeth.chakri26@gmail.com>
2024-10-31 01:34:44 +0530

    Day-1 Python Basics,String Manipulation,input Fucntion and short cuts

---
+++ Day-1/Day-1 Coding.py
@@ -0,0 +1,9 @@
+# Hello world - Sample
+print("Hello World !!\nHello World !!! \nHello World !!!!")
+# String Manipulation / Concatination
+print("Hello "+" Praneeth")
+
+#input("What is your Name?")
+#input fucntion ,expectes an input value
+# Short cut --to comment the line ctrl+/
+print("Name : "+input("What is your Name?")+" !")
