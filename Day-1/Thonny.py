3edd3f870eb61185f24262cf60eccd3356ffeda9
unknown <praneeth.chakri26@gmail.com>
2024-11-02 21:25:30 +0530

    Thonny IDE file for debugging, Day2 Topics and Project1

---
+++ Day-1/Thonny.py
@@ -0,0 +1,1 @@
+print("Welcome to Thonny IDE !! Python Learning!!")
---
+++ Day-2/Day-2 Coding.py
@@ -0,0 +1,30 @@
+##input("What is your Name? ")
+# Assigning the input value to variable name
+# name = input("What is your Name? ")
+# print(name)
+# print(len(name))
+
+# print(len(input("What is your Fav. Lang?")))
+# userName
+# length
+# user_Name = input("Enter UserName:")
+# length_Of_UserName = len(user_Name)
+# print(length_Of_UserName)
+
+# To check the length of String variable : len()
+# To pass dynamic input values to the program: input()
+# Assigning the value to the variables eg: name = input("UserName:")
+# Moving the curser to the next line after input by adding '\n' at the end of input("String \n")
+# Concatinating the varibles and pass those variabls to print function.
+
+## Band Generator - Task 1 from Day-1 Topics
+
+print("Welcome to the Band Name Generator")
+print("Let's Start !! ")
+
+name = input("What is your LastName?\n")
+
+town = input("What is Your HomeTown Name? \n")
+
+print("Your Band Name would be "+town+" "+name)
+
