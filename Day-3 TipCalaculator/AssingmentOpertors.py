b6008fbb0949033fbd78697c2e4539ba44047d23
unknown <praneeth.chakri26@gmail.com>
2024-11-11 00:36:12 +0530

    Assingment Operator

---
+++ Day-3 TipCalaculator/AssingmentOpertors.py
@@ -0,0 +1,11 @@
+score = 0
+
+score /= 1
+print(score)
+
+
+height = 1.56
+is_winning = True
+
+print(f"Welcome to the Fstring Topic, where im concatinating Integer to String + {score}, and floting Number {height} and Boolean as well{is_winning}")
+print(f" FString helps to avoid the unnecessary functions to convert the data type and while print ")
