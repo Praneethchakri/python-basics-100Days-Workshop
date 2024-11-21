778f0704f269fda4a0bb6702c5e05211710f0e29
unknown <praneeth.chakri26@gmail.com>
2024-11-22 01:24:32 +0530

    Practice Python Day-4 Still few topics left for the day

--- Day-3 TipCalaculator/AssingmentOpertors.py
+++ Day-3 TipCalaculator/AssingmentOpertors.py
@@ -1,11 +1,17 @@
 score = 0
 
-score /= 1
+# score /= 1
+score = score//1
 print(score)
 
 
 height = 1.56
 is_winning = True
 
-print(f"Welcome to the Fstring Topic, where im concatinating Integer to String + {score}, and floting Number {height} and Boolean as well{is_winning}")
+print(f"Welcome to the Fstring Topic, where im concatinating Integer to String + {score}, and floating Number {height} and Boolean as well{is_winning}")
 print(f" FString helps to avoid the unnecessary functions to convert the data type and while print ")
+
+
+print("Multiplication with Single *")
+print("Exponential with ** (Double start ) 2 ** 3 (2 Cube)")
+print("")
---
+++ Day-4 TreasureHunt/EvenOddCheckerWithModuloOperator.py
@@ -0,0 +1,6 @@
+print("Even Odd checker With Modulo Operator ")
+number = int(input("Enter the Number\n"))
+if number % 2 == 0:
+    print(f"{number} is Even Number ")
+else:
+    print(f"{number} is Odd Number")
