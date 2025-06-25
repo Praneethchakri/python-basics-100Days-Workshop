ca3107881b2ec33e7200ee37f4e502a14add06b1
DELL <praneeth.chakri26@gmail.com>
2025-06-26 00:50:03 +0530

    Python Flow Controls (if-else,elif,for-Loops,nested for-loops, break, continue Statements)

--- self-learning/DataTypes.py
+++ self-learning/DataTypes.py
@@ -35,3 +35,41 @@ set_DataType.add("Hanuma")
 print(set_DataType)
 
 
+print("Set Methods ")
+
+arts_students ={"Ramesh","Suresh","Kamal","Pramod"}
+science_Studnets = {"Pramod","Charan","Praneeth"}
+
+arts_students_not_in_Science = arts_students.difference(science_Studnets)
+print(arts_students_not_in_Science)
+science_Studnets_not_in_arts = science_Studnets.difference(arts_students) # difference is to remove the commen element in both sets
+print(science_Studnets_not_in_arts)
+
+arts_students_in_Science_both = arts_students.symmetric_difference(science_Studnets) #symetric_difference is to remove the common element in both the sets
+# and make one commone set
+print(arts_students_in_Science_both)
+arts_students_in_Science_both_intersection = arts_students.intersection(science_Studnets) # intersection method is used to find the common element from both sets
+print(arts_students_in_Science_both_intersection)
+
+arts_students_in_Science_both_union = arts_students.union(science_Studnets)
+print(arts_students_in_Science_both_union) # union method remove the duplicates and combine both sets
+
+
+
+
+batsmen = {"Dhoni","Sachin","Rahul","Yuvraj"}
+bowler = {"Zaheer","Jadeja","Yuvraj"}
+batsmenOnly = batsmen.difference(bowler)
+
+print(batsmenOnly)
+
+bowlerOnly = bowler.difference(batsmen)
+print(bowlerOnly)
+
+allRounder = bowler.intersection(batsmen) # common  element from both sets
+print(allRounder)
+
+print(bowler.symmetric_difference(batsmen)) # common element in both sets will be removed..
+
+unique_Players = bowler.union(batsmen)
+print(unique_Players) ## By removing the duplicates and keep  all the disticnt values
