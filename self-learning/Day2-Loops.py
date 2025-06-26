4a15dcdfdce6e1c793d1b7a778b57a35ae69adce
DELL <praneeth.chakri26@gmail.com>
2025-06-27 00:05:58 +0530

    Python Flow Controls (if-else,elif,for-Loops,nested for-loops, break, continue Statements)

--- self-learning/Day2-Loops.py
+++ self-learning/Day2-Loops.py
@@ -44,3 +44,36 @@ print(removeCommon)
 
 unionData = setData.union(cricData)
 print(unionData)
+
+
+
+# Dictionaries Data Type with key value pair model
+
+family_Details = {'Praneeth':33,'Sushma':31,'PrajnaSri':5}
+
+print(family_Details['Praneeth'])
+family_Details['Dhoni']=43
+family_Details['Sushma']=32
+print(family_Details)
+
+
+crickters = (
+    {"name":'Dhoni',"age":43}, # inside tuple we have 3 dictionaries
+    {"name": 'Sachin', "age": 53},
+    {"name": 'Rahul', "age": 33},
+)
+
+crickters = crickters+({'name':'Sehwag','age':48},)
+print(crickters)
+
+print(crickters[1]["name"])
+print(crickters[2]["age"])
+
+
+# if we have a list of tuples , to covert to dictonaries
+
+cric = [('Praneeth',3),('Sushma',2),('Saravani',3)]
+
+cricDictonaries = dict(cric)
+
+print(cricDictonaries)
