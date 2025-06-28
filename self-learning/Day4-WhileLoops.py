2c12f1aa48665f6599641f31971b02fb1e4e6584
DELL <praneeth.chakri26@gmail.com>
2025-06-29 02:01:42 +0530

    Python Flow Controls (if-else,elif,for-Loops,nested for-loops, break, continue Statements)

--- self-learning/Day4-WhileLoops.py
+++ self-learning/Day4-WhileLoops.py
@@ -5,5 +5,25 @@ while userInput == 'Yes':
     userInput = input("Still do you have focus.... Yes or No")
 print("Ok.. Take a Break!!! ComeBack Soon..")
 
+listOfDict = [
+    {'name': 'Praneeth', 'age': 33},
+    {'name': 'Sushma', 'age': 31},
+    {'name': 'PrajnaSri', 'age': 5},
+    {'name': 'P', 'age': 100}
+]
 
+# Practicing for Loop As Well...
+for person in listOfDict:
+    name = person['name']
+    age = person['age']
 
+    print(f'''{name} is {age} year's old''')
+
+list = [
+    ('Albert', 'Scientist', 'Germany', 89),
+    ('James', 'Scientist', 'Germany', 99),
+    ('Grahambell', 'Scientist', 'France', 79)
+]
+
+for name, profession, country, age in list:
+    print(f'{name} is a Great {profession} from {country} and {name} age is {age}')
