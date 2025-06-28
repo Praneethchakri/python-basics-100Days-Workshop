c6f6b171d6d6f6f32d0671e197f57e62cc2e2711
DELL <praneeth.chakri26@gmail.com>
2025-06-29 01:37:02 +0530

    Python Flow Controls (if-else,elif,for-Loops,nested for-loops, break, continue Statements)

---
+++ self-learning/Day4-WhileLoops.py
@@ -0,0 +1,9 @@
+userInput = input("Do you want to study well and understand the python? Yes or No")
+
+while userInput == 'Yes':
+    print("Good...Let's Focus... on it")
+    userInput = input("Still do you have focus.... Yes or No")
+print("Ok.. Take a Break!!! ComeBack Soon..")
+
+
+
