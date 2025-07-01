f1f7b7fb43db24f60e9afd19e75e6ef20a3637a1
DELL <praneeth.chakri26@gmail.com>
2025-07-01 22:30:58 +0530

    Python Flow Controls (if-else,elif,for-Loops,nested for-loops, break, continue Statements)

---
+++ self-learning/ListSlicing.py
@@ -0,0 +1,27 @@
+nameList = ['Praneeth', 'Diguvapalem', 'Chakravarthi', 'PrajnaSri', 'Sushma']
+# nameList=[  0,             1,            2,             3,          4   ]
+print(nameList[0:])
+print(nameList[1:])  # Ignore the first index elements
+
+print(nameList[2:])  # Ignore the first 2 indexes elements
+
+print(nameList[3:])  # Ignore first 3 indexes elements
+
+print(nameList[4:])  # Ignore fist 4 indexes elements
+
+print(nameList[5:])  # Ignore fist 5 indexes elements
+
+# print(nameList[:len(nameList) - 1])
+
+print(nameList[:5])  # will show all the elements in list
+print(nameList[:4])  # will show all the elements ,except last 1 elements
+print(nameList[:3])
+
+print(nameList[-3:4])
+
+print(nameList[-5:5])
+print(nameList[2:4])
+print(nameList[3:5])
+print(nameList[:-3])
+# print(nameList[4:-3])
+print(nameList[-2:])
