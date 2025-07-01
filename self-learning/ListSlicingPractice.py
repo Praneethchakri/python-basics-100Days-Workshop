8d924d37d4696b508e5ce42f459a9843b1a1c808
DELL <praneeth.chakri26@gmail.com>
2025-07-01 23:55:56 +0530

    Python Flow Controls (if-else,elif,for-Loops,nested for-loops, break, continue Statements)

---
+++ self-learning/ListSlicingPractice.py
@@ -0,0 +1,18 @@
+nameList = ['Praneeth', 'Diguvapalem', 'Chakravarthi', 'PrajnaSri', 'Sushma']
+
+print(nameList[1:])
+print(nameList[2:])
+print(nameList[:2])
+print(nameList[:-1])
+print(nameList[-1:])
+
+
+friends = ["Rolf", "Charlie", "Anna", "Bob", "Jen"]
+
+print(friends[2:4])# Anna ,bob
+print(friends[2:])#Anna,bob,jen
+print(friends[:4])#Rolf ,charli,anna ,
+print(friends[:]) #full list
+print(friends[-3:]) #anna ,bob jen
+print(friends[:-2])#Rolf,charlie anna
+print(friends[-3:-1]) #Anna ,bob
