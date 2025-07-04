f1f0655fc61f1dd79c94c61214e9f494517dcc5e
DELL <praneeth.chakri26@gmail.com>
2025-07-05 00:29:32 +0530

    Python Flow Controls (if-else,elif,for-Loops,nested for-loops, break, continue Statements)

---
+++ self-learning/BreakContinue.py
@@ -0,0 +1,8 @@
+names = ['nametst1', 'nametest22', 'nametest333', 'nametest444']
+
+for name in names:
+    if len(name) > 8:
+        print(f'Value length is > 6 {name}')
+    else:
+        continue
+
