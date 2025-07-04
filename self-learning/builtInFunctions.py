749c76b5d0646fa6487487d11c984f7da888d69d
DELL <praneeth.chakri26@gmail.com>
2025-07-05 00:29:46 +0530

    Python Flow Controls (if-else,elif,for-Loops,nested for-loops, break, continue Statements)

---
+++ self-learning/builtInFunctions.py
@@ -0,0 +1,6 @@
+# abs(): it returns the absolute value of a number
+# all(): It  reutrns true if all the items  in  an iterable objects are true.
+# any(): It Returns true if any of the item in an iterable ojbect is true
+# ascii() :Returns a readable version of an object . Replaces non ascii chars with escape character
+# bin(): returs the binary version of a number
+# bool(): Returns the boolean value of the specified object
