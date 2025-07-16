4e3f1e803ae1d074f98911d0519bb87664dcb938
DELL <praneeth.chakri26@gmail.com>
2025-07-17 00:05:59 +0530

    Python Flow Controls (if-else,elif,for-Loops,nested for-loops, break, continue Statements)

--- self-learning/LambdaWithFunctionCode.py
+++ self-learning/LambdaWithFunctionCode.py
@@ -1,17 +1,22 @@
-
-operations={
-    "avg" : lambda seq :sum(seq)/len(seq),
-    'max' : lambda seq:max(seq),
-    'total' : lambda seq : sum(seq)
+operations = {
+    "avg": lambda seq: sum(seq) / len(seq),
+    'max': lambda seq: max(seq),
+    'total': lambda seq: sum(seq)
 }
 
-
 students = [
-    {'name':'Praneeth','grades':{100,43,23,42,32,56}},
     {'name': 'Praneeth', 'grades': {100, 43, 23, 42, 32, 56}},
-    {  'name': 'Praneeth', 'grades': {100, 43, 23, 42, 32, 56}}
+    {'name': 'Suresh', 'grades': {89, 22, 23, 42, 32, 56}},
+    {'name': 'Mahesh', 'grades': {99, 23, 23, 42, 32, 56}}
 ]
 
-
 for student in students:
-    print(student)
+
+    name = student['name']
+    grade = student['grades']
+    print(f'Student Name: {name}')
+    operation = input("User Input as Avg or Max or total")
+
+    action = operations[operation]
+    print(action(grade))
+
