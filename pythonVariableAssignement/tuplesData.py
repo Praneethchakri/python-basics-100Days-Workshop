13389a139f96ac95b5d8e5d362a29afa250dbc88
DELL <praneeth.chakri26@gmail.com>
2026-02-03 23:09:07 +0530

    Python Flow Controls (if-else,elif,for-Loops,nested for-loops, break, continue Statements)

---
+++ pythonVariableAssignement/tuplesData.py
@@ -0,0 +1,21 @@
+print("Tuples:: Characterstics")
+print('''1.maintain insertion Ordered
+2.Immutable after creation
+3.Allow duplicate values
+4.Can store heterogenious data
+''')
+
+
+tuple1 = ('12','13',)
+print(tuple1)
+
+tuple2 = tuple1+('14',)
+print(tuple2)
+
+tuple2 = tuple2+(str('Praneeth'),)
+print(tuple2)
+
+tupleWithoutParathesis = 1,2,3,4,5
+print(tupleWithoutParathesis)
+
+
