2cf3e97f71e9bbfd1c9ff4c00fb86f5bc53ab8fd
DELL <praneeth.chakri26@gmail.com>
2026-02-03 23:14:29 +0530

    Python Flow Controls (if-else,elif,for-Loops,nested for-loops, break, continue Statements)

--- pythonVariableAssignement/tuplesData.py
+++ pythonVariableAssignement/tuplesData.py
@@ -18,4 +18,19 @@ print(tuple2)
 tupleWithoutParathesis = 1,2,3,4,5
 print(tupleWithoutParathesis)
 
+tupleWithSingleElement = (192,)
+print(tupleWithSingleElement)
+
+tupleConstructor = tuple([1,2,4])# Creating the tuple class object
+print(tupleConstructor)
+
+
+print()
+t = (1,2,3,4,5,6,7)
+print('negative indexing: ',t[-7])
+
+print('Slicing',t[1:4])
+print('indexing : ', t[3])
+
+
 
