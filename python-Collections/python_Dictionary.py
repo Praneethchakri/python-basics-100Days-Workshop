517c756830815ddc64ff40e37914443f305dbf1e
DELL <praneeth.chakri26@gmail.com>
2026-02-11 23:23:09 +0530

    Python Flow Controls (if-else,elif,for-Loops,nested for-loops, break, continue Statements)

---
+++ python-Collections/python_Dictionary.py
@@ -0,0 +1,17 @@
+friend_Details ={'Praneeth':35,'Ramesh':36,'Suresh':35}
+# value of name
+print(friend_Details['Praneeth'])
+
+# add new value to dictonary
+
+friend_Details['Kumar']=36
+print(friend_Details)
+
+# Dictionary will maintain the insertion order, but does not allow the duplicate key's
+
+
+friend_Details['Kumar']=46
+print(friend_Details)
+
+
+print(friend_Details)
