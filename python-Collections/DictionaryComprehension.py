e20c64bd08ff1aac023d35ae7585db30cf7388fd
DELL <praneeth.chakri26@gmail.com>
2026-02-21 17:13:36 +0530

    Python Flow Controls (if-else,elif,for-Loops,nested for-loops, break, continue Statements)

--- python-Collections/DictionaryComprehension.py
+++ python-Collections/DictionaryComprehension.py
@@ -7,6 +7,16 @@ team_Dic = {
     player_Name[i]:jersey_Number[i]
     for i in range(len(player_Name))
     if jersey_Number[i] < 8
-
 }
 print(f"Team Captains : ",team_Dic)
+
+
+
+# using Zip fuction to convert the set/list to dict
+
+
+result = dict(zip(player_Name,jersey_Number))
+print(f'Result as Dictionary :: {result}')
+
+resultAsList= list(zip(player_Name,jersey_Number))
+print(f'Result as List using Zip function which returns as List of Tuples :: {result}')
