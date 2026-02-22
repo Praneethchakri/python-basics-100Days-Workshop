a59b01165b90faca8957133bba8eac2b5247708c
DELL <praneeth.chakri26@gmail.com>
2026-02-22 16:47:09 +0530

    Python Flow Controls (if-else,elif,for-Loops,nested for-loops, break, continue Statements)

--- python-Collections/revision.py
+++ python-Collections/revision.py
@@ -28,3 +28,42 @@ print(zipResult)
 
 for zr in enumerate(zipResult,start=1):
     print(zr)
+
+    # This line loads a set of 6 random numbers from the config file
+    # lottery_numbers = config.lottery_numbers
+
+    # Here are your players; find out who has the most numbers matching lottery_numbers!
+    players = [
+        {"name": "Rolf", "numbers": {1, 3, 5, 7, 9, 11}},
+        {"name": "Charlie", "numbers": {2, 7, 9, 21, 10, 5}},
+        {"name": "Anna", "numbers": {13, 14, 15, 16, 17, 18}},
+        {"name": "Jen", "numbers": {19, 20, 12, 7, 3, 5}},
+    ]
+
+    # Then, print out a line such as "Jen won 1000.".
+    # The winnings are calculated with the formula:
+    # 100 ** len(numbers_matched)
+
+
+print(players)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
