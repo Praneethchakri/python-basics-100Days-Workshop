cb800fd8641af7ba5adbd349c70f85d6d0083f55
DELL <praneeth.chakri26@gmail.com>
2026-03-07 15:19:06 +0530

    Python Flow Controls (if-else,elif,for-Loops,nested for-loops, break, continue Statements)

---
+++ self-learning/Students.py
@@ -0,0 +1,14 @@
+class Students:
+    def __init__(self,name,address):
+        self.name=name
+        self.address = address
+
+    def printStudent(self):
+        return self.name+" <==> "+self.address
+
+
+student1 = Students("Praneeth","Germany") #once the object creation is done , the very next method to process or assign values to parameter is dunderinitmethod
+
+print(student1.printStudent())
+
+print(Students.printStudent(student1))
