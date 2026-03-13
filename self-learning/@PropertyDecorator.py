762fdb50c6ce4dbe1862c238dce0a58586253bf4
DELL <praneeth.chakri26@gmail.com>
2026-03-14 00:41:13 +0530

    Python Flow Controls (if-else,elif,for-Loops,nested for-loops, break, continue Statements)

---
+++ self-learning/@PropertyDecorator.py
@@ -0,0 +1,15 @@
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self._age = age
+
+
+    def __str__(self):
+        return f"{self.name} of age {self._age}"
+
+    @property # We can make a function to access as vairable using the @Property Decorator, , the function should have only self variable in method
+    def age(self):
+        return self._age
+
+p = Person("Praneeth", 34)
+print(p.age)
