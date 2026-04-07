6f1246ef3943c1adc6a32c9c5b9ae09eb6b30e2d
DELL <praneeth.chakri26@gmail.com>
2026-04-07 22:42:26 +0530

    Python Flow Controls (if-else,elif,for-Loops,nested for-loops, break, continue Statements)

--- self-learning/ErrorTypesPython.py
+++ self-learning/ErrorTypesPython.py
@@ -2,13 +2,25 @@
 # friends[2] will cause IndexError
 # KeyError: While accessing the key from dictionary if the value not found or missmatch
 # NameError :
-AttributeError
+# AttributeError
 # NotImplementedError :To Handle ongoing implementation to have user undestandable message
-RuntimeError
-SyntaxError
-IndentationError
-TabError
-TypeError
-ValueError
+# RuntimeError
+# SyntaxError
+# IndentationError
+# TabError
+# TypeError
+# ValueError
 # ImportError:
 # DeprecationWarning : DeprecationWarning bcoz of outdated features from library to show the warning while implementing
+
+
+class Garage:
+
+    def __init__(self):
+        self.car=[]
+
+
+    def __repr__(self):
+        return f"{self} Details"
+
+
